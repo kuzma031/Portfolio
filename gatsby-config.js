26d5c26cebdb97395ce5984eaf12c4ef75ef00d9
kuzma031 <kuzma031@yahoo.com>
2021-05-16 15:59:14 +0200

    git updated

---
+++ gatsby-config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    siteMetadata: {
+      title: `Đorđe Kuzmanović`,
+      description: `Full-Stack Software Developer. Expert in JavaScript, React and Node.js. `,
+      author: `Đorđe Kuzmanović`,
+    },
+    plugins: [
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-image`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
+      },
+      `gatsby-plugin-sass`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-anchor-links`,
+      `gatsby-plugin-transition-link`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          theme_color: `#663399`,
+          display: `minimal-ui`,
+          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        },
+      },
+      `gatsby-plugin-gatsby-cloud`,
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      // `gatsby-plugin-offline`,
+      {
+          resolve: `gatsby-plugin-google-gtag`,
+          options: {
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                process.env.GOOGLE_URL, // Google Analytics / GA
+              //   "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+              //   "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+              ],
+              // This object gets passed directly to the gtag config command
+              // This config will be shared across all trackingIds
+              // gtagConfig: {
+              //   optimize_id: "OPT_CONTAINER_ID",
+              //   anonymize_ip: true,
+              //   cookie_expires: 0,
+              // },
+              // This object is used for configuration specific to this plugin
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+              },
+          }
+      }
+    ],
+  }
+  
